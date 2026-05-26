e535050862f414f9878a8b23ca43bc9f1345eeee
Y3148y <14971050+y3148y@user.noreply.gitee.com>
2026-05-26 23:29:17 +0800

    使用lua脚本确保原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -16,6 +19,13 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取当前线程的id
@@ -28,7 +38,25 @@ public class SimpleRedisLock implements ILock{
         return Boolean.TRUE.equals(success); // 避免自动拆箱的空指针
     }
 
+    //基于Lua脚本释放锁，确保原子性
+    /**
+     * -- 比较线程标识与锁中的标识是否一致
+     * if(redis.call('get', KEYS[1]) == ARGV[1]) then
+     *   -- 释放锁
+     *   redis.call('del', KEYS[1])
+     * end
+     * return 0
+     */
+
     @Override
+    public void unLock() {
+        //调用Lua脚本 传参：lua脚本，keys，argv
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),
+                ID_PREFIX+Thread.currentThread().getId());
+    }
+    /*    @Override
     public void unLock() {
         //获取线程标识
         String threadId = ID_PREFIX+Thread.currentThread().getId();
@@ -39,4 +67,5 @@ public class SimpleRedisLock implements ILock{
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
     }
+ */
 }
